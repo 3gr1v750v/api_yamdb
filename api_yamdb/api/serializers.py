25c4a17e0c89980e272ac68596ff772c018bf5b5
EugeniGrivtsov <110385345+EugeniGrivtsov@users.noreply.github.com>
2023-01-24 15:01:40 +0300

    User authentication (#6)
    * Добавляет эндпоинт /api/v1/auth/signup/
    Эндпоинт принимает username и email, проверяет их по базе и отправляет письмо на указанный email, если пользователь уже зарегистрирован. Если пользователь не зарегистрирован, происходит регистрация и также отпарвляется письмо
    Используется эмуляция почтового сервера.
    
    * Добавляет эндпоинты для управления пользователями:
    -  /api/v1/users/ 
    - /api/v1/users/me
    Обновленны permissions
    
    * Дорабтка кода для прохождения тестов по регистрации и управлению пользователями.
    
    Co-authored-by: Eugeni Grivtsov <egivtsov@gmail.com>

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,7 +1,18 @@
-from rest_framework import serializers
+
+from rest_framework import serializers, status
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 
-from reviews.models import Title, Category, Genre, GenreTitle
+
+from reviews.models import Title, Category, Genre, GenreTitle, User
+from .utils import code_generator
+
+
+def get_tokens_for_user(user):
+    refresh = RefreshToken.for_user(user)
+
+    return {'refresh': str(refresh), 'access': str(refresh.access_token)}
 
 
 class CategorySerializer(serializers.ModelSerializer):
@@ -46,3 +57,41 @@ class TitleSerializer(serializers.ModelSerializer):
     class Meta:
         fields = ('pk', 'name', 'description', 'year', 'category', 'genre')
         model = Title
+
+
+class ConfirmationCodeSerailizer(serializers.ModelSerializer):
+    """Сериализатор для отправки пользователю кода подтверждения."""
+    class Meta:
+        model = User
+        fields = (
+            'username', 'email'
+        )
+
+
+class EmailAuthSerializer(serializers.Serializer):
+    """Сериализатор для получения токена по коду подтверждения."""
+    username = serializers.CharField(max_length=150)
+    confirmation_code = serializers.CharField(max_length=10)
+
+    def validate(self, data):
+        username = data['username']
+        user = get_object_or_404(User, username=username)
+
+        confirmation_code = data['confirmation_code']
+        confirmation_code_origin = code_generator(username)
+
+        if confirmation_code == confirmation_code_origin:
+            return get_tokens_for_user(user)
+        raise serializers.ValidationError(
+            {"message": ("Введённый код подтверждения не "
+                         "соответствут коду подтверждения пользователя.")})
+
+
+class UserSerializer(serializers.ModelSerializer):
+    """Сериализатор для эндпоинта user."""
+
+    class Meta:
+        model = User
+        fields = (
+            'username', 'email', 'first_name', 'last_name', 'bio', 'role'
+        )
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,12 +1,25 @@
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import viewsets, filters, mixins
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework import viewsets, filters, mixins, permissions
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.views import APIView
+from rest_framework import status
 
-from reviews.models import Title, Category, Genre
+
+from reviews.models import Title, Category, Genre, User
+from .utils import code_generator
+
 from .filters import TitleFilter
-from .permissions import IsAdminOrReadOnly
-from .serializers import TitleSerializer, CategorySerializer, GenreSerializer
+from .permissions import IsAdminOrReadOnly, IsProfileOwner, IsAdminOnly
+from .serializers import (
+    TitleSerializer,
+    CategorySerializer,
+    GenreSerializer,
+    ConfirmationCodeSerailizer,
+    UserSerializer)
+from .email import confirmation_code_email
 
 
 class TitleViewSet(viewsets.ModelViewSet):
@@ -47,3 +60,62 @@ class GenreViewSet(mixins.ListModelMixin, mixins.CreateModelMixin,
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name',)
 
+
+class ConfirmationCodeView(APIView):
+    """
+    Самостоятельная регистрация пользователя на портале.
+    Если данные прошли валидацию новый пользователь будет зарегистрирован и
+    получит письмо с кодом подтверждения. Если пользователь был
+    зарегистрирован ранее, он также получит письмо с кодом подтверждения.
+    """
+
+    permission_classes = (permissions.AllowAny,)
+
+    def post(self, request):
+        username = request.data.get("username")
+        email = request.data.get("email")
+        user = User.objects.filter(username=username, email=email)
+        if username:
+            confirmation_code = code_generator(username)
+        if user:
+            confirmation_code_email(email, confirmation_code)
+            return Response(request.data, status=status.HTTP_200_OK)
+        serializer = ConfirmationCodeSerailizer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        if serializer.is_valid():
+            serializer.save()
+            confirmation_code_email(email, confirmation_code)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    Эндпоинт для управления пользователями.
+    Можно осуществлять добавление и поиск по пользователям.
+    Пользователь может дополнить данные о себе через обращение к эндпоинту
+    /me/.
+    """
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = (IsAdminOnly, permissions.IsAuthenticated)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('=username',)
+    lookup_field = 'username'
+    http_method_names = ['get', 'post', 'patch', 'delete']
+
+    @action(
+        methods=['get', 'patch'],
+        detail=False,
+        url_name='get_user',
+        permission_classes=(IsProfileOwner, permissions.IsAuthenticated)
+    )
+    def me(self, request):
+        if request.method == 'PATCH':
+            serializer = UserSerializer(
+                request.user, data=request.data, partial=True
+            )
+            serializer.is_valid(raise_exception=True)
+            serializer.save(role=request.user.role)
+        serializer = UserSerializer(request.user)
+        return Response(serializer.data, status=status.HTTP_200_OK)
