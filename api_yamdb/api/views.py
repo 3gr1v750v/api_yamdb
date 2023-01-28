5a70bba8882a9670076a97b034491611e75db325
TiEnddd <113132942+TiEnddd@users.noreply.github.com>
2023-01-28 12:39:55 +0300

    Update views.py

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -204,7 +204,9 @@ class CommentViewSet(viewsets.ModelViewSet):
     ]
 
     def get_queryset(self):
-        review = get_object_or_404(Review, pk=self.kwargs.get("review_id"))
+        review = get_object_or_404(Review,  
+                                   id=self.kwargs.get("review_id"),
+                                   title__id=self.kwargs.get("title_id"))
         return review.comments.all()
 
     def perform_create(self, serializer):
