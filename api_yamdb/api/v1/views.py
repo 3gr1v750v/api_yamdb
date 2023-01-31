1964505d8159300a8d5b5cb122561081130b2492
Eugeni Grivtsov <egivtsov@gmail.com>
2023-01-31 09:51:11 +0300

    Version 3 fine_tuning

--- api_yamdb/api/v1/views.py
+++ api_yamdb/api/v1/views.py
@@ -4,7 +4,7 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters, mixins, permissions, status, viewsets
 from rest_framework.decorators import action
 from rest_framework.pagination import PageNumberPagination
-from rest_framework.permissions import IsAuthenticatedOrReadOnly, AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from reviews.models import Category, Genre, Review, Title
